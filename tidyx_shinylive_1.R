e6f544e076af80d535022a33e6b6643908833a50
Ellis Hughes <ellishughes@live.com>
2023-10-20 13:18:43 -0700

    first commit

---
+++ tidyx_shinylive_1.R
@@ -0,0 +1,20 @@
+
+## the cars folder contains last weeks shiny app (bit.ly/TidyX_Ep160)
+## Convert your shiny app into all the assets for running the app 
+## in the browser
+shinylive::export(
+  appdir = "cars",
+  destdir = "docs"
+  )
+
+
+## with development version of httpuv, run shinylive app locally
+## remotes::install_github("rstudio/httpuv")
+httpuv::runStaticServer(
+  dir = "docs", 
+  port = 8888,
+  background = TRUE
+  )
+
+
+
